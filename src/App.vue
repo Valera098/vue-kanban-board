26e61de3190c9576a5f3197a58c05986e218e38c
Constancia565656 <Valera098Main@mail.ru>
2020-07-12 13:46:26 +0300

    Added modal not fully-functional

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
       </div>
     </div>
     <div class="row mt-5">
-      <div class="col-3">
+      <div class="col-4">
         <div class="p-2 alert alert-secondary">
           <h3>ToDo</h3>
           <draggable
@@ -33,7 +33,7 @@
         </div>
       </div>
 
-      <div class="col-3">
+      <div class="col-4">
         <div class="p-2 alert alert-primary">
           <h3>In Progress</h3>
           <draggable
@@ -50,36 +50,13 @@
               {{ element.content }}
               <h6> Start time </h6>
               {{ element.start }}
-              <b-button class="btn-sm" @click="edit(element)"> <b-icon-gear-fill></b-icon-gear-fill> </b-button>
+              <b-button class="btn-sm" @click="editItem(element)"> <b-icon-gear-fill></b-icon-gear-fill> </b-button>
             </div>
           </draggable>
         </div>
       </div>
 
-      <div class="col-3">
-        <div class="p-2 alert alert-warning">
-          <h3>Verifying</h3>
-          <draggable
-            class="list-group kanban-column"
-            :list="toVerify"
-            group="tasks"
-          >
-            <div @dragend="save"
-              class="list-group-item"
-              v-for="element in toVerify"
-              :key="element.content"
-            >
-              <h5>Task #{{ element.header }}</h5>
-              {{ element.content }}
-              <h6> Start time </h6>
-              {{ element.start }}
-              <b-button class="btn-sm" @click="edit(element)"> <b-icon-gear-fill></b-icon-gear-fill> </b-button>
-            </div>
-          </draggable>
-        </div>
-      </div>
-
-      <div class="col-3">
+      <div class="col-4">
         <div class="p-2 alert alert-success">
           <h3>Done</h3>
           <draggable
@@ -98,15 +75,46 @@
               {{ element.start }}
               <h6> End time </h6>
               {{ element.end}}
-              <b-button class="btn-sm" @click="edit(element)"> <b-icon-gear-fill></b-icon-gear-fill> </b-button>
+              <b-button class="btn-sm" @click="editItem(element)"> <b-icon-gear-fill></b-icon-gear-fill> </b-button>
             </div>
           </draggable>
         </div>
       </div>
     </div>
+    <b-modal id="editModal" title="Edit item">
+      <form>
+        <div class="modal-body">
+          <div class="form-group">
+              <label>Description</label>
+              <b-form-input type="text" class="form-control" v-model="modalContent" :value="modalContent">
+              </b-form-input>
+          </div>
+          <div class="form-group">
+              <label>Status</label>
+              <b-form-select class="form-control">
+
+              </b-form-select>
+          </div>
+          <div class="form-group">
+              <label>Doer</label>
+              <b-form-input type="text" class="form-control">
+              </b-form-input>
+          </div>
+          <div class="form-group">
+              <label>Start time</label>
+              <b-form-input type="date" class="form-control"></b-form-input>
+              <b-form-input type="time" class="form-control"></b-form-input>
+          </div>
+          <div class="form-group">
+              <label>End time</label>
+              <b-form-input type="date" class="form-control"></b-form-input>
+              <b-form-input type="time" class="form-control"></b-form-input>
+          </div>
+        </div>
+      </form>
+    </b-modal>
   </div>
 </template>
-
 <script>
 import draggable from "vuedraggable";
 import { BIconGearFill } from 'bootstrap-vue';
@@ -124,40 +132,52 @@ export default {
     let itemsLog = JSON.parse(localStorage.getItem('itemsLog'));
 
     if (itemsLog){
-      return {
+        this.toDo = itemsLog[0];
+        this.inProgress = itemsLog[1];
+        this.done = itemsLog[2];
+    }
+    else {
+      this.toDo = [];
+      this.inProgress = [];
+      this.done = [];
+    }
+    return {
+        toDo: this.toDo,
+        inProgress: this.inProgress,
+        done: this.done,
         newTask: "",
-        toDo: itemsLog[0],
-        inProgress: itemsLog[1],
-        toVerify: itemsLog[2],
-        done: itemsLog[3]
-      }
-    } else {
-      return {
-        newTask: "",
-        toDo: [],
-        inProgress: [],
-        toVerify: [],
-        done: []
-      }
+        modalContent: "",
+        modalStatus: "",
+        modalDoer: "",
+        modalStartDate: "",
+        modalStartTime: "",
+        modalEndDate: "",
+        modalEndTime: ""
     }
   },
   methods: {
+    save: function(){
+      localStorage.setItem('itemsLog',JSON.stringify(
+        [ this.toDo, this.inProgress, this.done ]));
+        },
     add: function() {
       if (this.newTask) {
         let lastHeader = +localStorage.getItem('lastHeader');
         lastHeader += 1;
         localStorage.setItem('lastHeader',lastHeader);
         this.toDo.push({ header: lastHeader,
-        content: this.newTask, doer: null, start: null, end: null });
+        content: this.newTask, doer: "", start: new Date(), end: new Date(), status: "toDo"});
         this.newTask = "";
+        this.save();
       }
     },
-    save: function(){
-      localStorage.setItem('itemsLog',JSON.stringify(
-        [ this.toDo, this.inProgress, this.toVerify, this.done ]));
-        },
-    edit: function(element){
+    editItem: function(element){
       this.selectedItem = element;
+      this.modalContent = element.content;
+      this.modalStatus = element.status;
+      this.modalDoer = element.doer;
+
+      this.$bvModal.show('editModal')
     }
   }
 };
